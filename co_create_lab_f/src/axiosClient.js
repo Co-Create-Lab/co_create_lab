034cb0a429050cb161570195ec69a35e3fba43b6
SvjetlanaR <svjetlanarados@gmail.com>
2023-03-07 19:39:55 +0100

    removed api

--- co_create_lab_f/src/axiosClient.js
+++ co_create_lab_f/src/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: process.env.REACT_APP_BE_URL || "/api",
+  baseURL: process.env.REACT_APP_BE_URL || "",
   withCredentials: true,
 });
 
